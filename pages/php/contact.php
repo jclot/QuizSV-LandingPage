4ef8acf917033a500e2d0d7f63d916bd942c6e51
Julian Clot <JulianClot@Julians-MacBook-Pro.local>
2020-12-03 03:04:13 -0600

    create an contact php email sender algorithm

---
+++ pages/php/contact.php
@@ -0,0 +1,15 @@
+<?php 
+ 
+ $first_name = $_POST['first_name'];
+ $last_name = $_POST['last_name'];
+ $email = $_POST['email'];
+ $phone = $_POST['phone'];
+ $message = $_POST['message'];
+ $formcontent = " From: $first_name \n $last_name \n \n Phone: $phone \n \n Message: $message";
+ $recipient = "juliclot123@gmail.com";
+ $subject = "Contact Form";
+ $mailheader = "From: $email \r\n";
+ mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+ echo "Thank You!" . " -" . "<a href='../../index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+
+?>
