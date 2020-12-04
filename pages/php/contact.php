14f139a2b1ad03fab1377df689722239cc1acb8a
Julian Clot <JulianClot@Julians-MacBook-Pro.local>
2020-12-04 17:27:19 -0600

    redirect to a page ones submited the form

--- pages/php/contact.php
+++ pages/php/contact.php
@@ -10,6 +10,6 @@
  $subject = "Contact Form";
  $mailheader = "From: $email \r\n";
  mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
- echo "Thank You!" . " -" . "<a href='../../index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+ header('Location: ../Help.html');
 
 ?>
